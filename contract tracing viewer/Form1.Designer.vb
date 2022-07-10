2a5dbe474e298c8a44829f92f6c6fc7355790fb2
steven <steven17@gmail.com>
2022-07-11 03:16:13 +0800

    added data grid view

--- contract tracing viewer/Form1.Designer.vb
+++ contract tracing viewer/Form1.Designer.vb
@@ -25,6 +25,10 @@ Partial Class Form1
         Me.lbl1 = New System.Windows.Forms.ListBox()
         Me.txtmybox = New System.Windows.Forms.TextBox()
         Me.btnsearch = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'lbl1
@@ -47,23 +51,48 @@ Partial Class Form1
         'btnsearch
         '
         Me.btnsearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.btnsearch.Location = New System.Drawing.Point(359, 214)
+        Me.btnsearch.Location = New System.Drawing.Point(360, 307)
         Me.btnsearch.Name = "btnsearch"
         Me.btnsearch.Size = New System.Drawing.Size(107, 87)
         Me.btnsearch.TabIndex = 2
-        Me.btnsearch.Text = "search"
+        Me.btnsearch.Text = "import"
         Me.btnsearch.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(360, 12)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 25
+        Me.DataGridView1.Size = New System.Drawing.Size(520, 289)
+        Me.DataGridView1.TabIndex = 3
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(473, 343)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(41, 15)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Label1"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1091, 546)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.DataGridView1)
         Me.Controls.Add(Me.btnsearch)
         Me.Controls.Add(Me.txtmybox)
         Me.Controls.Add(Me.lbl1)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -72,4 +101,7 @@ Partial Class Form1
     Friend WithEvents lbl1 As ListBox
     Friend WithEvents txtmybox As TextBox
     Friend WithEvents btnsearch As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Label1 As Label
 End Class
--- contract tracing viewer/Form1.vb
+++ contract tracing viewer/Form1.vb
@@ -1,12 +1,24 @@
 ﻿Imports System.IO
 Public Class Form1
+
+    Dim table As New DataTable()
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim mypath() As Object = Directory.GetFiles("C:\Users\User\source\repos\contract tracing viewer\contract tracing viewer\bin\Debug")
+        Dim mypath() As Object = Directory.GetFiles("C:\Users\User\source\repos\contract tracing viewer\contract tracing viewer\bin\Debug", "*.txt")
         For Each item As String In mypath
             Dim rslt As String = Path.GetFileName(item)
             lbl1.Items.Add(rslt)
 
         Next
+
+
+        table.Columns.Add("name", Type.GetType("System.String"))
+        table.Columns.Add("covid", Type.GetType("System.String"))
+        table.Columns.Add("gender", Type.GetType("System.String"))
+        table.Columns.Add("age", Type.GetType("System.String"))
+
+
+        DataGridView1.DataSource = table
+
     End Sub
 
     Private Sub btnsearch_Click(sender As Object, e As EventArgs) Handles btnsearch.Click
@@ -17,5 +29,28 @@ Public Class Form1
             myreader = My.Computer.FileSystem.OpenTextFileReader(src)
             txtmybox.Text = myreader.ReadToEnd()
         End If
+
+
+
+        If OpenFileDialog1.ShowDialog = DialogResult.OK Then
+            Label1.Text = OpenFileDialog1.FileName
+        End If
+
+        Dim lines() As String
+        Dim vals() As String
+
+        lines = File.ReadAllLines(Label1.Text)
+
+        For i As Integer = 0 To lines.Length - 1 Step +1
+            vals = lines(i).ToString().Split("|")
+            Dim row(vals.Length - 1) As String
+
+            For j As Integer = 0 To vals.Length - 1 Step +1
+                row(j) = vals(j).Trim()
+
+            Next j
+            table.Rows.Add(row)
+        Next i
+
     End Sub
 End Class
