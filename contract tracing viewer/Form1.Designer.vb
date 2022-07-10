6376b95125f6040d44276490e695f17eeaa236cd
steven <steven17@gmail.com>
2022-07-11 02:43:18 +0800

    added all codes and files

---
+++ contract tracing viewer/Form1.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.lbl1 = New System.Windows.Forms.ListBox()
+        Me.txtmybox = New System.Windows.Forms.TextBox()
+        Me.btnsearch = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lbl1
+        '
+        Me.lbl1.FormattingEnabled = True
+        Me.lbl1.ItemHeight = 15
+        Me.lbl1.Location = New System.Drawing.Point(12, 12)
+        Me.lbl1.Name = "lbl1"
+        Me.lbl1.Size = New System.Drawing.Size(303, 289)
+        Me.lbl1.TabIndex = 0
+        '
+        'txtmybox
+        '
+        Me.txtmybox.Location = New System.Drawing.Point(12, 327)
+        Me.txtmybox.Multiline = True
+        Me.txtmybox.Name = "txtmybox"
+        Me.txtmybox.Size = New System.Drawing.Size(303, 92)
+        Me.txtmybox.TabIndex = 1
+        '
+        'btnsearch
+        '
+        Me.btnsearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnsearch.Location = New System.Drawing.Point(359, 214)
+        Me.btnsearch.Name = "btnsearch"
+        Me.btnsearch.Size = New System.Drawing.Size(107, 87)
+        Me.btnsearch.TabIndex = 2
+        Me.btnsearch.Text = "search"
+        Me.btnsearch.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnsearch)
+        Me.Controls.Add(Me.txtmybox)
+        Me.Controls.Add(Me.lbl1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lbl1 As ListBox
+    Friend WithEvents txtmybox As TextBox
+    Friend WithEvents btnsearch As Button
+End Class
---
+++ contract tracing viewer/Form1.vb
@@ -0,0 +1,21 @@
+﻿Imports System.IO
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim mypath() As Object = Directory.GetFiles("C:\Users\User\source\repos\contract tracing viewer\contract tracing viewer\bin\Debug")
+        For Each item As String In mypath
+            Dim rslt As String = Path.GetFileName(item)
+            lbl1.Items.Add(rslt)
+
+        Next
+    End Sub
+
+    Private Sub btnsearch_Click(sender As Object, e As EventArgs) Handles btnsearch.Click
+        Dim myreader As System.IO.StreamReader
+        Dim src As String = txtmybox.Text + ".txt"
+
+        If File.Exists(src) Then
+            myreader = My.Computer.FileSystem.OpenTextFileReader(src)
+            txtmybox.Text = myreader.ReadToEnd()
+        End If
+    End Sub
+End Class
